b09ece39de883620ea72bf6f38b3d86c5b6f5415
Saniya Siddiqui <saniyasiddiqui894@gmil.com>
2026-09-24 02:49:40 +0530

    first commit

---
+++ ServerCreate-Day2/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send('Hello World')
+})
+
+app.get('/about', (req,res)=>{
+    res.send('This is About Page')
+})
+
+app.get('/home', (req,res)=>{
+    res.send('This is Home Page')
+})
+
+app.listen(3000)
